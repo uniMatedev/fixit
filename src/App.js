961a6001ed63b24cb71bb31734b444abbdcd0588
uniMate <zachmbanks@gmail.com>
2021-06-15 10:09:54 -0700

    changes for v0.01

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>fixIt</h1>
+        <h1>San Diego</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
